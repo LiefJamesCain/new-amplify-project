21e24b6505ad99bb97d0161348033dff954f129f
liefjamescain <liefjamescain@gmail.com>
2019-11-23 15:45:06 -0800

    fbgoogle-signin COMPLETE

---
+++ amplify-auth-demo/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import {Auth} from 'aws-amplify';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <button onClick={() => Auth.federatedSignIn()}>Sign In</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
